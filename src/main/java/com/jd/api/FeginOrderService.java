67599cd4538ba9ee0c0fa945eadc58bd8ef62354
handou26 <251585741@qq.com>
2019-02-17 17:07:05 +0800

    Update FeginOrderService.java

--- src/main/java/com/jd/api/FeginOrderService.java
+++ src/main/java/com/jd/api/FeginOrderService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(value="orderService",fallback=FeginOrderError.class)
 public interface FeginOrderService {
 	
-	 @RequestMapping(value = "/getPatients")
+     @RequestMapping(value = "/getPatients")
      public String getPatients();
 
 }
